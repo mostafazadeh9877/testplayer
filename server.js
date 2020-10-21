fde3f6a05f8e837b97756785d1581abc56db36c4
Ali <ali.mostafazadeh77@gmail.com>
2020-10-21 13:08:13 +0330

    first commit

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const http = require('http').createServer(app);
 const io = require('socket.io').listen(http);
 require('dotenv').config();
 
+app.get('/', (req, res) => {
+    res.end();
+});
+
 app.get('/:divisionName/:version', (req, res) => {
     let divisionName = req.params.divisionName;
     let version = req.params.version;
@@ -19,30 +23,30 @@ app.get('/:divisionName/:version', (req, res) => {
 io.on('connection', function (socket) {
     console.log('create a new connection');
     let now = new Date().getTime();
-    const scheduleList = [
+    const firstTestList = [
         {
-            url: "http://localhost:3000/first/1.0.0/",
+            url: "http://localhost:3000/firstDivision/1.0.0/",
             date: "yyyy/mm/dd",
             start: now,
-            end: now+60000,
-        },{
+            end: now + 60000,
+        }, {
             url: "http://localhost:3000/secondDivision/2.0.0/",
             date: "yyyy/mm/dd",
-            start: now+60000,
-            end: now+120000,
+            start: now + 60000,
+            end: now + 120000,
         },{
-            url: "http://localhost:3000/first/1.0.0/",
+            url: "http://localhost:3000/firstDivision/1.0.0/",
             date: "yyyy/mm/dd",
-            start: now+120000,
-            end: now+180000,
-        },{
+            start: now + 120000,
+            end: now + 180000,
+        }, {
             url: "http://localhost:3000/secondDivision/2.0.0/",
             date: "yyyy/mm/dd",
-            start: now+180000,
-            end: now+240000,
+            start: now + 180000,
+            end: now + 240000,
         }
     ];
-    io.emit("newScheduleList",scheduleList);
+    socket.emit("new Schedule List", firstTestList);
 });
 
 http.listen(process.env.PORT, (err) => {
